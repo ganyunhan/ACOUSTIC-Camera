dfe4b918654a16486b570b19026afb3f57ba517e
程皓 <1759870950@qq.com>
2022-09-15 16:42:51 +0800

    improve format and  add annotation

--- project/src/rtl/cal_position/cal_position.v
+++ project/src/rtl/cal_position/cal_position.v
@@ -5,78 +5,33 @@ module cal_position(
     output reg [15:0] x_position,
     output reg [15:0] y_position,
     output wire [15:0] z_position,
-    output wire cal_end
+    output wire cal_end//caculate end
 
 );
 
-localparam L=7'd100;   // mm
-localparam vel=34;   // mm/0.1ms
+localparam L=7'd100;   // distance between microphones (mm)
+localparam vel=34;   //  velicity of sound (mm/0.1ms)
 
-reg [15:0] R;
-wire [15:0] dist12,dist13,dist14;
-wire [31:0] z_position2;
+reg [15:0] R;       //distance between target and microphones
+wire [15:0] dist12,dist13,dist14;        //d12,d13,d14
+wire [31:0] z_position2;                //z*z
 
-assign dist12=vel*delay12;
+assign dist12=vel*delay12;              // d=v*t
 assign dist13=vel*delay13;
 assign dist14=vel*delay14;
 
 always@(posedge ena) begin
     if(!cal_end)begin
-    R<=(dist13*dist13-dist12*dist12-dist14*dist14)/(2*(dist12+dist14-dist13));
-    x_position<=(2*R*dist12+dist12*dist12)/(2*L);
-    y_position<=(2*R*dist14+dist14*dist14)/(2*L);
+        R<=(dist13*dist13-dist12*dist12-dist14*dist14)/(2*(dist12+dist14-dist13));   //R=(d13^2-d12^-d14^2)/2(d12+d14-d13)
+        x_position<=(2*R*dist12+dist12*dist12)/(2*L);                                //x=(2*R*d12+d12^2)/2L
+        y_position<=(2*R*dist14+dist14*dist14)/(2*L);                                //y=(2*R*d14+d14^2)/2L
     end
     else begin
-    x_position<=x_position;
-    y_position<=y_position;
+        x_position<=x_position;
+        y_position<=y_position;
     R<=R;    
     end
 end
-assign z_position2=R*R-(x_position-L/2)*(x_position-L/2)+(y_position-L/2)*(y_position-L/2);
-sqrt mysqrt(z_position2,fast_clk,z_position,cal_end);
-endmodule
-
-module sqrt(
-    input wire [31:0] in_data,
-    input clk,
-    output reg [15:0] out_data,
-    output reg sqrt_end=0
-);
-localparam times=31;
-reg [7:0]cnt;
-reg [15:0] h_tempdata,l_tempdata,tempdata;
-reg [3:0] state=0;
-always @(posedge clk)begin
-    case(state)
-        0:begin 
-            state<=1;
-            cnt<=0;
-            h_tempdata<=16'd65535;
-            l_tempdata<=16'b0;
-            tempdata<=1516;
-            sqrt_end<=0;
-        end
-        1:begin
-            if(cnt<=times)begin
-                cnt+=1;
-                if(tempdata*tempdata>in_data)begin
-                    h_tempdata<=tempdata;
-                end
-                else begin
-                    l_tempdata<=tempdata;
-                end
-                tempdata<=(h_tempdata+l_tempdata)/2; 
-            end
-            else begin
-                state<=2;
-                out_data<=tempdata;
-                sqrt_end<=1;
-            end
-        end
-        2:begin
-            state<=0;
-        end
-        default:state<=0;
-    endcase
-end
+assign z_position2=R*R-(x_position-L/2)*(x_position-L/2)-(y_position-L/2)*(y_position-L/2);//z^2=R^2-(x-L/2)^2-(y-L/2)^2
+sqrt mysqrt(z_position2,fast_clk,z_position,cal_end);                                      //z=sqrt (z^2)
 endmodule
--- project/src/rtl/cal_position/sqrt.v
+++ project/src/rtl/cal_position/sqrt.v
@@ -1,42 +1,42 @@
-module sqrt(
+module sqrt(       //使用二分法计算开根号
     input wire [31:0] in_data,
     input clk,
     output reg [15:0] out_data,
     output reg sqrt_end=0
 );
-localparam times=31;
-reg [7:0]cnt;
-reg [15:0] h_tempdata,l_tempdata,tempdata;
+localparam times=31;    //max iteration times
+reg [7:0]cnt;           //iteration counter
+reg [15:0] h_tempdata,l_tempdata,tempdata;   //data used in the iteration
 reg [3:0] state=0;
 always @(posedge clk)begin
     case(state)
         0:begin 
             state<=1;
             cnt<=0;
-            h_tempdata<=16'd65535;
-            l_tempdata<=16'b0;
-            tempdata<=1516;
-            sqrt_end<=0;
+            h_tempdata<=16'd65535;          //begining high data    
+            l_tempdata<=16'b0;              //begining low data   
+            tempdata<=1516;                 //begining mid data random
+            sqrt_end<=0;                    //the flag of caculation
         end
         1:begin
-            if(cnt<=times)begin
+            if(cnt<=times)begin                     //Less than the max iteration times
                 cnt+=1;
-                if(tempdata*tempdata>in_data)begin
-                    h_tempdata<=tempdata;
+                if(tempdata*tempdata>in_data)begin  //tempdata>target     =>    tempdata>target>l_tempdata
+                    h_tempdata<=tempdata;           
                 end
                 else begin
-                    l_tempdata<=tempdata;
+                    l_tempdata<=tempdata;           //tempdata<target     =>tempdata<target<h_tempdata
                 end
-                tempdata<=(h_tempdata+l_tempdata)/2; 
+                tempdata<=(h_tempdata+l_tempdata)/2; //二分，取l_tempdata和h_tempdata中点
             end
             else begin
-                state<=2;
+                state<=2;                            //arrive the max iteration times
                 out_data<=tempdata;
-                sqrt_end<=1;
+                sqrt_end<=1;                         //caculation ends
             end
         end
         2:begin
-            state<=0;
+            state<=0;                               //reset
         end
         default:state<=0;
     endcase
