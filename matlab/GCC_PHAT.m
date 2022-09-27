8a9737fa011b5c48655a0794bbc580d5da06afef
ganyunhan <hank.gan@foxmail.com>
2022-09-27 23:48:12 +0800

    Changing the logic to fit the test cases

--- matlab/GCC_PHAT.m
+++ matlab/GCC_PHAT.m
@@ -2,30 +2,23 @@ clear;
 clc;
 close all;
 
-o_x="..\project\data\x_512.txt";  %%待处理文件路径
-o_y="..\project\data\y_512.txt";  %%待处理文件路径
+o_x="..\Module_test\XCORR\data\x_data.dat";  %%待处理文件路径
+o_y="..\Module_test\XCORR\data\y_data.dat";  %%待处理文件路径
 
-% o_x="..\Module_test\XCORR\data\x.txt";  %%待处理文件路径
-% o_y="..\Module_test\XCORR\data\y.txt";  %%待处理文件路径
+data_x = dlmread(o_x);
+data_y = dlmread(o_y);
 
-data_ori_x = dlmread(o_x);
-data_ori_y = dlmread(o_y);
+golden_result_f = '..\Module_test\XCORR\data\golden_result.dat';
+golden_result = dlmread(golden_result_f);
 
-tb_x = dec2bin(dlmread(o_x),17);
-tb_y = dec2bin(dlmread(o_y),17);
+[result1,lags1] = xcorr(data_x,data_y,10);
 
-data_x = B2QW(bin2dec(tb_x),17);
-data_y = B2QW(bin2dec(tb_y),17);
-% [result,lags] = myxcorr(data_x,data_y,1024);
-% result = result';
-% [M1,I1] = max(abs(result));% 模仿 Matlab doc 给出延迟坐标
-% plot(lags,result);
+figure(1);
+plot(result1);
+title('Test result');
 
-[result1,lags1] = xcorr(data_ori_x,data_ori_y,3);
+figure(2);
+plot(golden_result);
+title('Golden result');
 
-data_x_diff = data_ori_x - data_x;
-data_y_diff = data_ori_y - data_y;
-%result2 = result' - result1;
-% [M2,I2] = max(abs(result1));% 模仿 Matlab doc 给出延迟坐标
-% figure(2)
-% plot(lags1,result1);
+err = (result1 - golden_result')/result1';
