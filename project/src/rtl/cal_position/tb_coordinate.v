85c2725f87fc823a76e67a8f70decefb50384202
0entropy0 <74545002+0entropy0@users.noreply.github.com>
2022-09-30 15:35:08 +0800

    add quadrant judgement

--- project/src/rtl/cal_position/tb_coordinate.v
+++ project/src/rtl/cal_position/tb_coordinate.v
@@ -8,37 +8,48 @@ reg  [15:0] y;
 reg  [15:0] z;
 wire  [15:0] x_2d;
 wire  [15:0] y_2d;
+reg [1:0] quadrant;
 initial begin
         $display("start a clock pulse");    // 打印开始标记
         $dumpfile("test_coordinate.vcd");              // 指定记录模拟波形的文件
         $dumpvars(0, test_coordinate);          // 指定记录的模块层级
         clock <= 1;
         x<=40;
-        y<=160;
-        z<=240;                         // 初始clock信号为1
+        y<=20;
+        z<=160;
+        quadrant<=2'b0;                         // 初始clock信号为1
         #6000 $finish;                  // 6000个单位时间后结束模拟
 
     end
     always begin
-        #20 clock =~clock;
+        #40 clock =~clock;
     end
     always begin
-        #1000 x<=100;y<=100;
+        #1000 x<=0;y<=0;
     end
-    coordinate_3DTO2D trans(x,y,z,clock,x_2d,y_2d);
+    always begin
+        #10 quadrant<=quadrant+1;
+    end
+    coordinate_3DTO2D trans(x,y,z,quadrant,clock,x_2d,y_2d);
 endmodule
 module coordinate_3DTO2D(
     input[15:0] x,
     input[15:0] y,
     input[15:0] z,
+    input[1:0]quadrant,                                         //定义象限信息声源来自第quadrant-1象限
     input clk,
     output[15:0] x_2d,
     output[15:0] y_2d
 );
 integer i,j;
 
-localparam rate=7'd20; //height/156 or width/208
+localparam height=2900; //height/156 or width/208
+localparam weight=4000;
+wire [7:0] rate;
 reg [9:0] IntrinsicMatrix [0:2][0:2];
+wire [15:0] temp_x;                     //store the coordinate without adding quadrant information
+wire [15:0] temp_y;
+assign rate=weight/208;
 always @(posedge clk) begin
     IntrinsicMatrix[0][0]<=185;
     IntrinsicMatrix[0][1]<=0;
@@ -51,7 +62,8 @@ always @(posedge clk) begin
     IntrinsicMatrix[2][2]<=1;
    
 end    
-assign x_2d=(IntrinsicMatrix[0][0]*x+IntrinsicMatrix[0][1]*y+IntrinsicMatrix[0][2]*z)/z*rate;
-assign y_2d=(IntrinsicMatrix[1][0]*x+IntrinsicMatrix[1][1]*y+IntrinsicMatrix[1][2]*z)/z*rate;
-
+assign temp_x=(IntrinsicMatrix[0][0]*x+IntrinsicMatrix[0][1]*y+IntrinsicMatrix[0][2]*z)/z*rate;
+assign temp_y=(IntrinsicMatrix[1][0]*x+IntrinsicMatrix[1][1]*y+IntrinsicMatrix[1][2]*z)/z*rate;
+assign x_2d=(~quadrant[0]^quadrant[1])?temp_x:weight-temp_x;
+assign y_2d=quadrant[1]?temp_y:height-temp_y;
 endmodule
