7ccdc14767d5d316a53b5bc51b3711859fb25ecf
Akun <1677968721@qq.com>
2022-10-08 14:57:33 +0800

    Modify vfb_tmp.v

--- Module_test/Camera/src/video_process/video_frame_buffer/video_frame_buffer_tmp.v
+++ Module_test/Camera/src/video_process/video_frame_buffer/video_frame_buffer_tmp.v
@@ -14,17 +14,20 @@
 		.I_dma_clk(I_dma_clk_i), //input I_dma_clk
 		.I_wr_halt(I_wr_halt_i), //input [0:0] I_wr_halt
 		.I_rd_halt(I_rd_halt_i), //input [0:0] I_rd_halt
+		
 		.I_vin0_clk(I_vin0_clk_i), //input I_vin0_clk
 		.I_vin0_vs_n(I_vin0_vs_n_i), //input I_vin0_vs_n
 		.I_vin0_de(I_vin0_de_i), //input I_vin0_de
 		.I_vin0_data(I_vin0_data_i), //input [15:0] I_vin0_data
 		.O_vin0_fifo_full(O_vin0_fifo_full_o), //output O_vin0_fifo_full
+
 		.I_vout0_clk(I_vout0_clk_i), //input I_vout0_clk
 		.I_vout0_vs_n(I_vout0_vs_n_i), //input I_vout0_vs_n
 		.I_vout0_de(I_vout0_de_i), //input I_vout0_de
 		.O_vout0_den(O_vout0_den_o), //output O_vout0_den
 		.O_vout0_data(O_vout0_data_o), //output [15:0] O_vout0_data
 		.O_vout0_fifo_empty(O_vout0_fifo_empty_o), //output O_vout0_fifo_empty
+
 		.I_cmd_ready(I_cmd_ready_i), //input I_cmd_ready
 		.O_cmd(O_cmd_o), //output [2:0] O_cmd
 		.O_cmd_en(O_cmd_en_o), //output O_cmd_en
